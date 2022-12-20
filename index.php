cc4df0739a46ad26e03548f5d7ca0788fafe9ba5
falenzhika <ffalenzhika02@gmail.com>
2022-12-20 23:28:14 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+echo "Hello World! <br>";
+
+$nama = "Falenzhika";
+$umur = 21;
+
+echo "Nama saya <strong> $nama </strong>, saya berusia $umur tahun.<br>";
+
+$namaKakak = "Enzhika";
+$umurKakak = 28;
+
+echo "Nama Kakak saya <strong>$namaKakak</strong>, kakak saya berusia $umurKakak tahun. <br>";
+
+$selisihUmur = $umurKakak - $umur;
+echo "Selisih usia saya dan kakak saya adalah $selisihUmur tahun";
+
+
+?>
